df7287f0a9888a467251cd47323e2ba953d9509d
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-04-17 19:48:26 +0700

    feat: add dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import notesRoute from "./routes/notes.routes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/v1/notes", notesRoute);
 
-app.listen(5001, () => {
-  console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
 });
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://ghanifaustaraihan_db_user:pPs0IOt3qkyqTz7q@cluster0.wrtyhbr.mongodb.net/?appName=Cluster0",
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
