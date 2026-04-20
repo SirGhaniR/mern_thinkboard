97ad0bf74423a0f284613f41e2ff39f153197165
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-04-20 19:36:46 +0700

    feat: make note controller functional

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,12 +1,22 @@
-export const getNotes = (req, res) => {
+import Note from "../models/Note.js";
+
+export const getNotes = async (req, res) => {
+  const notes = await Note.find();
+
   return res.status(200).json({
     success: "true",
     message: "Notes has been fetched successfully",
+    data: notes,
   });
 };
 
-export const createNote = (req, res) => {
-  const { note } = req.body;
+export const createNote = async (req, res) => {
+  const { title, content } = req.body;
+
+  const note = await Note.create({
+    title: title,
+    content: content,
+  });
 
   return res.status(201).json({
     success: "true",
@@ -15,17 +25,30 @@ export const createNote = (req, res) => {
   });
 };
 
-export const getNote = (req, res) => {
-  const { id } = req.params.id;
+export const getNote = async (req, res) => {
+  const id = req.params.id;
+
+  const note = await Note.findById(id);
+
   return res.status(200).json({
     success: "true",
     message: "Note has been fetched successfully",
-    data: id,
+    data: note,
   });
 };
 
-export const updateNote = (req, res) => {
-  const { note } = req.body;
+export const updateNote = async (req, res) => {
+  const id = req.params.id;
+  const { title, content } = req.body;
+
+  const note = await Note.findByIdAndUpdate(
+    id,
+    {
+      title: title,
+      content: content,
+    },
+    { returnDocument: "after" },
+  );
 
   return res.status(200).json({
     success: "true",
@@ -34,11 +57,14 @@ export const updateNote = (req, res) => {
   });
 };
 
-export const deleteNote = (req, res) => {
-  const { id } = req.params.id;
+export const deleteNote = async (req, res) => {
+  const id = req.params.id;
+
+  const note = await Note.findByIdAndDelete(id);
+
   return res.status(200).json({
     success: "true",
-    message: "Note has been fetched successfully",
-    data: id,
+    message: "Note has been deleted successfully",
+    data: note,
   });
 };
