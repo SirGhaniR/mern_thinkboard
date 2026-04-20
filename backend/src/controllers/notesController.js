5d408dc02bf1a6ebca4397729f21d7f414828fc8
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-04-20 20:20:04 +0700

    feat: use trycatch block and proper 404 handling

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,70 +1,142 @@
 import Note from "../models/Note.js";
 
 export const getNotes = async (req, res) => {
-  const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort by newest first (desc. order)
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort by newest first (desc. order)
 
-  return res.status(200).json({
-    success: "true",
-    message: "Notes has been fetched successfully",
-    data: notes,
-  });
+    if (!notes) {
+      return res.status(404).json({
+        success: false,
+        message: "Notes not found",
+        data: notes,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Notes has been fetched successfully",
+      data: notes,
+    });
+  } catch (error) {
+    console.error("Error in getNotes:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Internal Error",
+    });
+  }
 };
 
 export const createNote = async (req, res) => {
-  const { title, content } = req.body;
+  try {
+    const { title, content } = req.body;
 
-  const note = await Note.create({
-    title: title,
-    content: content,
-  });
+    const note = await Note.create({
+      title: title,
+      content: content,
+    });
 
-  return res.status(201).json({
-    success: "true",
-    message: "Notes has been created successfully",
-    data: note,
-  });
+    return res.status(201).json({
+      success: true,
+      message: "Notes has been created successfully",
+      data: note,
+    });
+  } catch (error) {
+    console.error("Error in createNote:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Internal Error",
+    });
+  }
 };
 
 export const getNote = async (req, res) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const note = await Note.findById(id);
+    const note = await Note.findById(id);
 
-  return res.status(200).json({
-    success: "true",
-    message: "Note has been fetched successfully",
-    data: note,
-  });
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: note,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Note has been fetched successfully",
+      data: note,
+    });
+  } catch (error) {
+    console.error("Error in getNote:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Internal Error",
+    });
+  }
 };
 
 export const updateNote = async (req, res) => {
-  const id = req.params.id;
-  const { title, content } = req.body;
+  try {
+    const id = req.params.id;
+    const { title, content } = req.body;
 
-  const note = await Note.findByIdAndUpdate(
-    id,
-    {
-      title: title,
-      content: content,
-    },
-    { returnDocument: "after" },
-  );
+    const note = await Note.findByIdAndUpdate(
+      id,
+      {
+        title: title,
+        content: content,
+      },
+      { returnDocument: "after" },
+    );
 
-  return res.status(200).json({
-    success: "true",
-    message: "Notes has been updated successfully",
-    data: note,
-  });
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: note,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Notes has been updated successfully",
+      data: note,
+    });
+  } catch (error) {
+    console.error("Error in updateNote:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Internal Error",
+    });
+  }
 };
 
 export const deleteNote = async (req, res) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const note = await Note.findByIdAndDelete(id);
+    const note = await Note.findByIdAndDelete(id);
 
-  return res.status(200).json({
-    success: "true",
-    message: "Note has been deleted successfully",
-    data: note,
-  });
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: note,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Note has been deleted successfully",
+      data: note,
+    });
+  } catch (error) {
+    console.error("Error in deleteNote:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Internal Error",
+    });
+  }
 };
